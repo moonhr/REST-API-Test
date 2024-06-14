12f0128a9855296f0c68f38b7602f25a3655def1
moonhr <moonhr153@gmail.com>
2024-06-14 11:28:39 +0900

    주제변경으로 인한 url 변경

--- src/controller.ts
+++ src/controller.ts
@@ -1,8 +1,9 @@
-const endpoint = `http://apis.data.go.kr/1400000/forestStusService`
+const endpoint = `http://apis.data.go.kr/6300000/kickBoard`
 
 const keyName = `SP6poFNj53tVrUmPFA7YiAuohTBnOXQ%2BUE4J6uq0ItPV49i%2Bu2%2FVXlNUnq5v3WumEyZthjVOj9wG%2FotkQW%2F5kg%3D%3D`;
 
-const url = `${endpoint}/getfirestatsservice?serviceKey=${keyName}&numOfRows=1000&pageNo=1&searchStDt=20240101&searchEdDt=20240613`;
+const url = `${endpoint}/getkickBoard?serviceKey=${keyName}&pageNo=1&numOfRows=1000`;
+
 
 console.log(url);
 
@@ -17,11 +18,7 @@ const AJAX = () => {
     if (xhr.status === 200) {
       console.log("데이터 받은 후 조회")
     
-      //* 얘 왜 안돼?
-      // const result = JSON.parse(xhr.responseText)
-      //* 얘 왜 돼?
-      const result = new DOMParser().parseFromString(xhr.responseText, 'text/xml')
-
+      const result = JSON.parse(xhr.responseText)
       console.log(result)
     }
   }
