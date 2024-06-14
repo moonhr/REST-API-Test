17e91df8978cb93b0c71bc92202187c004cca95a
moonhr <moonhr153@gmail.com>
2024-06-14 10:38:50 +0900

    new mimeType 모듈화

--- server/app.js
+++ server/app.js
@@ -1,1 +1,28 @@
-console.log("메롱");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const mimeType = require("./mimeType.js");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return;
+  let filePath = fileUtils.getFilePath(req.url);
+  let ext = fileUtils.getFileExtension(filePath);
+  let contentType = fileUtils.getContentType(ext);
+  if (req.method === "GET") {
+    getMethod(req, res, filePath, contentType);
+  }
+})
+
+function getMethod(req, res, filePath, contentType) {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("404 Not Found: 요청하신 파일을 찾을 수 없습니다.");
+      console.log("오류 발생:", err);
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    }
+  });
+}
+
---
+++ server/mimeType.js
@@ -0,0 +1,11 @@
+export const mimeType = {
+  ".html": "text/html; charset=utf-8",
+  ".css": "text/css",
+  ".js": "application/javascript",
+  ".json": "application/json",
+  ".ico": "image/x-icon",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".png": "image/png",
+  ".gif": "image/gif",
+};
