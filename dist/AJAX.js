de9dc2ed25f95e1fcc7b8c373fdb7a8e23623419
moonhr <moonhr153@gmail.com>
2024-06-14 14:59:53 +0900

    ajax 에러추가

--- dist/AJAX.js
+++ dist/AJAX.js
@@ -4,23 +4,28 @@ import { MakeLi } from "./MakeLi.js";
  * * ajax
  */
 export const AJAX = () => {
-    const main = document.getElementById("main");
-    const xhr = new XMLHttpRequest();
-    console.log(xhr);
-    xhr.open('GET', url, true);
-    xhr.onload = () => {
-        if (xhr.status === 200) {
-            console.log("데이터 받은 후 조회");
-            const result = JSON.parse(xhr.responseText);
-            console.log(result.response.body.items);
-            const items = result.response.body.items;
-            const ul = document.createElement("ul");
-            MakeLi(items, ul);
-            const div = document.createElement("div");
-            console.log("2");
-            div.appendChild(ul);
-            main.appendChild(div);
-        }
-    };
-    xhr.send();
+    try {
+        const main = document.getElementById("main");
+        const xhr = new XMLHttpRequest();
+        console.log(xhr);
+        xhr.open('GET', url, true);
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                console.log("데이터 받은 후 조회");
+                const result = JSON.parse(xhr.responseText);
+                console.log(result.response.body.items);
+                const items = result.response.body.items;
+                const ul = document.createElement("ul");
+                MakeLi(items, ul);
+                const div = document.createElement("div");
+                console.log("2");
+                div.appendChild(ul);
+                main.appendChild(div);
+            }
+        };
+        xhr.send();
+    }
+    catch (err) {
+        console.log(err);
+    }
 };
--- src/AJAX.ts
+++ src/AJAX.ts
@@ -1,33 +1,38 @@
-import {url} from "./url.js"
-import {MakeLi} from "./MakeLi.js"
+import { url } from "./url.js"
+import { MakeLi } from "./MakeLi.js"
 
 /**
  * * ajax
  */
 export const AJAX = () => {
-  const main = document.getElementById("main")!;
-  const xhr = new XMLHttpRequest()
-  console.log(xhr);
-  xhr.open('GET', url, true)
+  try {
+    const main = document.getElementById("main")!;
+    const xhr = new XMLHttpRequest()
+    console.log(xhr);
+    xhr.open('GET', url, true)
 
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      console.log("데이터 받은 후 조회")
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        console.log("데이터 받은 후 조회")
 
-      const result = JSON.parse(xhr.responseText)
-      console.log(result.response.body.items)
+        const result = JSON.parse(xhr.responseText)
+        console.log(result.response.body.items)
 
-      const items: string[] = result.response.body.items
-      const ul = document.createElement("ul")
-      
-      MakeLi(items, ul);
+        const items: string[] = result.response.body.items
+        const ul = document.createElement("ul")
 
-      const div = document.createElement("div");
+        MakeLi(items, ul);
 
-      console.log("2")
-      div.appendChild(ul);
-      main.appendChild(div);
+        const div = document.createElement("div");
+
+        console.log("2")
+        div.appendChild(ul);
+        main.appendChild(div);
+      }
     }
+    xhr.send();
   }
-  xhr.send();
+  catch (err) {
+    console.log(err)
+  }
 }
