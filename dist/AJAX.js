427e3be3555a68617aa51b3f7b5739d886e71c09
moonhr <moonhr153@gmail.com>
2024-06-14 14:45:14 +0900

    ajax 모듈화

---
+++ dist/AJAX.js
@@ -0,0 +1,25 @@
+import { url } from "./url.js";
+import { MakeLi } from "./MakeLi.js";
+const main = document.createElement("main");
+/**
+ * * ajax
+ */
+export const AJAX = () => {
+    const xhr = new XMLHttpRequest();
+    console.log(xhr);
+    xhr.open('GET', url, true);
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            console.log("데이터 받은 후 조회");
+            const result = JSON.parse(xhr.responseText);
+            console.log(result.response.body.items);
+            const items = result.response.body.items;
+            const ul = document.createElement("ul");
+            MakeLi(items, ul);
+            const div = document.createElement("div");
+            div.appendChild(ul);
+            main.appendChild(div);
+        }
+    };
+    xhr.send();
+};
--- dist/controller.js
+++ dist/controller.js
@@ -1,29 +1,8 @@
-import { url } from "./url.js";
-import { MakeLi } from "./MakeLi.js";
+import { AJAX } from "./AJAX.js";
 const root = document.getElementById('root');
 const main = document.createElement("main");
 root.appendChild(main);
 const button = document.createElement("button");
 button.textContent = "대전광역시 전동킥보드 주차장";
 main.appendChild(button);
-const AJAX = () => {
-    const xhr = new XMLHttpRequest();
-    console.log(xhr);
-    xhr.open('GET', url, true);
-    xhr.onload = () => {
-        if (xhr.status === 200) {
-            console.log("데이터 받은 후 조회");
-            const result = JSON.parse(xhr.responseText);
-            console.log(result.response.body.items);
-            //items 타입 선언 해야함
-            const items = result.response.body.items;
-            const ul = document.createElement("ul");
-            MakeLi(items, ul);
-            const div = document.createElement("div");
-            div.appendChild(ul);
-            main.appendChild(div);
-        }
-    };
-    xhr.send();
-};
 button.addEventListener('click', AJAX);
