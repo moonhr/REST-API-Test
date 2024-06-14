587ad9c9153231363fd97558003a0cd18b4555da
moonhr <moonhr153@gmail.com>
2024-06-14 11:03:48 +0900

    REST API 엔드포인트를 사용하여 url 생성

--- dist/controller.js
+++ dist/controller.js
@@ -1,2 +1,6 @@
 "use strict";
 console.log('메롱');
+const endpoint = `http://apis.data.go.kr/1400000/forestStusService`;
+const keyName = `SP6poFNj53tVrUmPFA7YiAuohTBnOXQ%2BUE4J6uq0ItPV49i%2Bu2%2FVXlNUnq5v3WumEyZthjVOj9wG%2FotkQW%2F5kg%3D%3D`;
+const url = `${endpoint}/getfirestatsservice?serviceKey=${keyName}&numOfRows=1000&pageNo=1&searchStDt=20240101&searchEdDt=20240613`;
+console.log(url);
